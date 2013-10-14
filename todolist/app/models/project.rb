8fecc6ee12aacb628c32c26225a6ab1a6df5ca4e
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2013-10-14 10:35:55 -0300

    adiciona required para nome e title, que estavam faltando

--- todolist/app/models/project.rb
+++ todolist/app/models/project.rb
@@ -1,5 +1,8 @@
 class Project < ActiveRecord::Base
   has_many :tasks
   attr_accessible :name, :tasks_attributes
+
+  validates :name, :presence => true
+
   accepts_nested_attributes_for :tasks, :allow_destroy => true, :reject_if => :all_blank
 end
--- todolist/app/models/task.rb
+++ todolist/app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :project
   attr_accessible :title, :completed
+
+  validates :title, :presence => true
 end
