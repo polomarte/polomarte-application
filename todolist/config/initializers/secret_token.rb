d979c06d008d36317f9fda2de2d6b9976e4c9a86
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2013-10-14 11:35:12 -0300

    volta secret_token e .rspec altera de novo o .gitignore (  prestar mais atenção quanto a isso )

---
+++ todolist/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = 'e34be64728a5c10c1e8e9455542f9285408d1b31ad359c74a7fe041d5a6736c32b23c4b0b3fb0759e97e8aa4a54cabfca87b1065a51df54a0ea5db6479d2c24f'
