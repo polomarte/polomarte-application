d1c99275ce334bad3e278c12c005d93889cecbe0
Marcos Teixeira <m.viniteixeira@gmail.com>
2013-10-14 07:47:33 -0300

    Refactoring

--- polomarte-project/app/controllers/tasks_controller.rb
+++ polomarte-project/app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:update, :destroy]
+
   def create
     @task = Task.create!(params[:task].permit(:description, :finalized, :project_id))
     redirect_to project_path @task.project
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update_attributes!(params[:task].permit(:description, :finalized, :project_id))
     respond_to do |format|
       format.html { redirect_to project_path @task.project }
@@ -14,7 +15,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.destroy(params[:id])
+    @task.destroy
     respond_to do |format|
       format.html { redirect_to project_path @task.project }
       format.js
@@ -22,6 +23,11 @@ class TasksController < ApplicationController
   end
 
   private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   # Never trust parameters from the scary internet, only allow the white list through.
   def task_params
     params.require(:task).permit(:description, :finalized, :project_id)
