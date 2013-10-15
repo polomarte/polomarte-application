c646f6ca26e35e765b3d304720e65f4239f567cd
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2013-10-15 01:06:21 -0300

    adiciona callback para verificar se o projeto foi concluido

--- todolist/app/models/project.rb
+++ todolist/app/models/project.rb
@@ -1,8 +1,13 @@
 class Project < ActiveRecord::Base
   has_many :tasks
-  attr_accessible :name, :tasks_attributes
+  attr_accessible :name, :tasks_attributes, :completed
 
   validates :name, :presence => true
 
   accepts_nested_attributes_for :tasks, :allow_destroy => true, :reject_if => :all_blank
+
+  before_save do
+    self.completed = true
+    tasks.each { |task| self.completed &&= task.completed }
+  end
 end
