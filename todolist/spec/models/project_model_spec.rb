25b1aceb9c38988aec331493176b802573e1a64b
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2013-10-15 10:02:32 -0300

    teste validate_completed, garante que só é completo com todas as tasks completas e garante que é incompleto quando não possui tasks

--- todolist/spec/models/project_model_spec.rb
+++ todolist/spec/models/project_model_spec.rb
@@ -8,4 +8,33 @@ describe Project do
   it { should allow_mass_assignment_of :tasks_attributes }
 
   it { should have_many :tasks }
+
+  let(:project) { FactoryGirl.build :project }
+
+  describe '#validate_completed' do
+    before(:each) do
+      project.tasks << FactoryGirl.build_list(:task, 2)
+      project.save
+    end
+
+    it 'should_not_be_completed_with_uncompleted_tasks' do
+      project.tasks.each { |task| task.completed = false; task.save}
+      project.tasks.first.completed = true
+      project.tasks.first.save
+      project.save!
+      project.should_not be_completed
+    end
+
+    it 'should_be_completed_with_only_completed_tasks' do
+      project.tasks.each { |task| task.completed = true; task.save}
+      project.save!
+      project.should be_completed
+    end
+
+    it 'should_be_completed_with_no_tasks' do
+      project.tasks.delete
+      project.save!
+      project.should_not be_completed
+    end
+  end
 end
