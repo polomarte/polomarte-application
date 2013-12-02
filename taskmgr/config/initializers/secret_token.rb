c2ac7bc81055c77f7df8656e1bf5ba910d91bc47
Felippe <raposo.felippe@gmail.com>
2013-12-02 09:51:28 -0200

    Initial commit

---
+++ taskmgr/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taskmgr::Application.config.secret_key_base = '7ac96e76a80e24c9d986b49f3ca2469b76fe97bd6fae2230c26901f097b6df28505f8a0f92bfe24ce35b778b8f0f19006c4e34e5c3388507eba359d9d4492431'
---
+++ taskmgr/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Taskmgr::Application.config.session_store :cookie_store, key: '_taskmgr_session'
