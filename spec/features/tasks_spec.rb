0e1da1f43a23cf6d0571cdb14e20dbecc57218fd
queirozfcom <falmeida1988@gmail.com>
2013-12-12 03:19:22 -0200

    uns testes melhorezinhos...

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -12,18 +12,32 @@ describe "Tasks" do
 
   end
 
-end
 
-describe 'Tasks create' do
+  describe 'Tasks create' do
 
-    it "should not be possible to add a task to an unexisting project" do
+    it "should be possible to add a task to the first available project given theres at least one project" do
+
+
+      Project.where(:name => 'meu-projeto-teste').first_or_create
 
       visit '/tasks/new'
 
-
-
       expect(page).to have_content('Nova Tarefa')
 
+      fill_in "task_name", :with => 'foo bar baz'
+      select 'alta', :from => 'task_priority_id'
+
+      select 'meu-projeto-teste', from: 'task_project_id'
+
+      fill_in "task_content", :with => 'lorem ipsum'
+
+      click_button 'Criar Tarefa'
+
+      expect(page).to have_content "Task was successfully created."
+
     end
+
+  end
+
 
 end
