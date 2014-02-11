cbc59fe86078599965e8edaaaaf67b48416bee6f
rafaelorosco <raforosco@gmail.com>
2014-02-11 14:33:46 -0200

    first commit

---
+++ app/views/projects/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @project, :id, :nome, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Polomarte::Application.config.secret_key_base = '7eb9d37d5866a64a7a004af35517b62f772301a6f47f551a215b52f7cda04e2678908dfaabad5d526e81a7fda47bae8b3170fb5b7ea96a6f41b64995b1bdf121'
